e082976eead10a7bdeeaf773306610a5dbe18522
choiJunHo <wnsghqwe@gmail.com>
2022-09-19 11:11:37 +0900

    feat: csv parser 생성 및 통신 / parser 테스트

--- lib/data/source/remote/stock_api.dart
+++ lib/data/source/remote/stock_api.dart
@@ -4,12 +4,12 @@ class StockApi {
   static const baseUrl = "https://www.alphavantage.co/";
   static const apiKey = "K0JHUAB56C3JZ8IW";
 
-  final http.Client client;
+  final http.Client _client;
 
-  StockApi(this.client);
+  StockApi({http.Client? client}) : _client = (client ?? http.Client());
 
   Future<http.Response> getListings({String apikey = apiKey}) async {
-    return await client.get(Uri.parse(
+    return await _client.get(Uri.parse(
         "https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=$apiKey"));
   }
 }
---
+++ test/data/source/remote/stock_api_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:us_stock_app/data/csv/company_listings_parser.dart';
+import 'package:us_stock_app/data/source/remote/stock_api.dart';
+
+void main() {
+  test("네트워크 통신", () async {
+    final _parser = CompanyListingsParser();
+
+    final response = await StockApi().getListings();
+
+    final remoteListings = await _parser.parse(response.body);
+
+    expect(remoteListings[0].symbol, "A");
+    expect(remoteListings[0].name, "Agilent Technologies Inc");
+    expect(remoteListings[0].exchange, "NYSE");
+  });
+}
