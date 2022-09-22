324be195deab2aa503a12022e374eb32b8bfc083
choiJunHo <wnsghqwe@gmail.com>
2022-09-22 10:36:03 +0900

    feat: viewModel 과 UI 를 연결 / 불필요한 연산을 하지 않도록 debounce 적용
    debounce => 의도적으로 함수의 호출 속도를 늦춰 짧은 시간에 너무많은 작동을 하는 것을 방지

--- lib/presentation/company_listings/company_listings_view_model.dart
+++ lib/presentation/company_listings/company_listings_view_model.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:us_stock_app/domain/repository/stock_repository.dart';
+import 'package:us_stock_app/presentation/company_listings/company_listings_action.dart';
 import 'package:us_stock_app/presentation/company_listings/company_listings_state.dart';
 
 class CompanyListingsViewModel with ChangeNotifier {
@@ -7,10 +10,28 @@ class CompanyListingsViewModel with ChangeNotifier {
 
   var _state = CompanyListingsState();
 
+  // 디바운스 용도
+  Timer? _debounce;
+
   CompanyListingsState get state => _state;
 
   CompanyListingsViewModel(this._repository) {
     _getCompanyListings();
+  }
+
+  void onAction(CompanyListingsAction action) {
+    action.when(
+      refresh: () => _getCompanyListings(fetchFromRemote: true),
+      onSearchQueryChange: (query) {
+        // 디바운스 구현 => 너무 빨리 많은 작동을 하는 것을 방지
+        // 디바운스에 아직 명령이 있다면 디바운스에 적용된 함수가 작동되지 않음
+        _debounce?.cancel();
+        // 앞의 시간(500 밀리초) 이 후에 뒤의 함수가 call 되도록 명령
+        _debounce = Timer(const Duration(milliseconds: 500), () {
+          _getCompanyListings(query: query);
+        });
+      },
+    );
   }
 
   Future _getCompanyListings(
