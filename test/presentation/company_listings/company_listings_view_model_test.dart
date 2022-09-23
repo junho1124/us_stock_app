6291297b31a836e7913ebc65a2fff69d16ca4501
choiJunHo <wnsghqwe@gmail.com>
2022-09-23 11:18:07 +0900

    refactor: refactor

--- test/presentation/company_listings/company_listings_view_model_test.dart
+++ test/presentation/company_listings/company_listings_view_model_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:us_stock_app/data/source/local/company_listing_entity.dart';
 import 'package:us_stock_app/data/source/local/stock_dao.dart';
 import 'package:us_stock_app/data/source/remote/stock_api.dart';
@@ -12,8 +10,7 @@ import 'package:us_stock_app/presentation/company_listings/company_listings_view
 void main() {
   test("company_listings_view_model 생성 시 데이터를 잘 가져와야 한다.", () async {
     WidgetsFlutterBinding.ensureInitialized();
-    final directory = await getApplicationDocumentsDirectory();
-    Hive.init(directory.path);
+    Hive.initFlutter();
     Hive.registerAdapter(CompanyListingEntityAdapter());
     final _api = StockApi();
     final _dao = StockDao();
